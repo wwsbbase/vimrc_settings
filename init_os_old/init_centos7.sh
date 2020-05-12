7cb101d395ffca89e674700584d435ae1330a2e3
D.J. Zou <bopy2008@163.com>
2020-05-12 09:29:09 +0800

    整理目录结构
    整理目录结构

--- init_os_old/init_centos7.sh
+++ init_os_old/init_centos7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #####		一键初始化CentOS7		 #####
-#####		Update:2020-5-12		#####
+#####		Update:2019-11-11		#####
 
 
 # 文件夹结构
@@ -23,8 +23,8 @@ function BaseSetting()
 	#sudo visudo
 
 	########## Base Setting ###########
-	SetHostname
-	SetConsoleColor
+	ChangeHostname
+	ChangeConsoleColor
 
 	# 安装字符集
 	# locale-gen en_US.UTF-8
@@ -32,137 +32,19 @@ function BaseSetting()
 	echo '----------------------------------'
 }
 
-###
-# Set host name
-###
-function SetHostname()
+function ChangeHostname()
 {
+	# set host name
 	sudo hostnamectl set-hostname $system_hostname
 	sudo echo "127.0.1.1   ${system_hostname}" >> /etc/hosts
 }
 
-
-###
-# Set Console Color
-###
-function SetConsoleColor()
+function ChangeConsoleColor()
 {
 	# set PS1
 	echo $setUserColor >> $userFolder/.bashrc
 	sudo echo $setRootColor >> /root/.bashrc
 }
-
-###
-# Close selinux services
-###
-function CloseSelinxServices()
-{
-	/bin/sed -i 's/mingetty tty/mingetty --noclear tty/' /etc/inittab
-	/bin/sed -i 's/SELINUX=permissive/SELINUX=disabled/' /etc/selinux/config
-	/bin/sed -i 's/SELINUX=enforcing/SELINUX=disabled/'  /etc/selinux/config
-}
-
-###
-# Close unuseful services
-###
-function CloseUnusefulServices()
-{
-	systemctl disable 'postfix'
-	systemctl disable 'NetworkManager'
-	systemctl disable 'abrt-ccpp'
-}
-
-###
-# Public key
-###
-function SetPublicKey()
-{
-	mkdir /root/.ssh
-	pub_key='ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4mvukv4f5seBuzrCnCCm1DpSgYw/kvq+XgsUP8mnzUpyaQ6D8BKfbOn6T20tUU/ksiJwSuUQHfw5v9JsnBACto3o/RmId0Ltn4DCq19sSwMP3YJb9dRb8SA/Pc5Xl7MPwPoSYyuY20ztMfo1GBx5N9dDuQ3j1MdKYTY9SdfFwPr0ZQvesKT1ozfQ9HHrcUi1CLJw+irYW9+jU39CsMrrZmCjb/n53gP77Do0lj9TkqXK2SYNdA88cmK2IQJP3LfFWWrwYH01FkImZbt7ODDQ21BqGccLY7xCbsNaniBlT8Mpy4/Wlg1qqnNPxBbw1nrs9A+2MnAfGDHXYhkFC/n6wQ== root@linux.jesonc.net'
-	echo $pub_key >> /root/.ssh/authorized_keys
-	chmod 700 /root/.ssh
-	chmod 600 /root/.ssh/authorized_keys
-	chown -R root:root /root/.ssh
-}
-
-###
-# Sysctl config 
-###
-function SysctlConfig()
-{
-
-found=`grep -c net.ipv4.tcp_tw_recycle /etc/sysctl.conf`
-if ! [ $found -gt "0" ]
-then
-cat > /etc/sysctl.conf << EOF
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-fs.file-max = 131072
-kernel.panic=1
-net.ipv4.tcp_rmem = 4096 87380 16777216
-net.ipv4.tcp_wmem = 4096 65536 16777216
-net.ipv4.tcp_timestamps = 0
-net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_no_metrics_save = 1
-net.core.netdev_max_backlog = 3072
-net.ipv4.tcp_max_syn_backlog = 4096
-net.ipv4.tcp_max_tw_buckets = 720000
-net.ipv4.ip_local_port_range = 1024 65000
-net.ipv4.tcp_fin_timeout = 5
-net.ipv4.tcp_tw_recycle = 1
-net.ipv4.tcp_retries1 = 2
-net.ipv4.tcp_retries2 = 10
-net.ipv4.tcp_synack_retries = 2
-net.ipv4.tcp_syn_retries = 2
-net.ipv4.tcp_syncookies = 1
-EOF
-fi
-
-sysctl -p
-}
-
-
-###
-# Max open files
-###
-function SetMaxOpenFiles()
-{
-	found=`grep -c "^* soft nproc" /etc/security/limits.conf`
-	if ! [ $found -gt "0" ]
-	then
-cat >> /etc/security/limits.conf << EOF
-* soft nproc 2048
-* hard nproc 16384
-* soft nofile 8192
-* hard nofile 65536
-EOF
-	fi
-}
-
-###
-# ssh config
-###
-/bin/sed -i 's/.*Port[[:space:]].*$/Port 9922/' /etc/ssh/ssh_config
-/bin/sed -i 's/.*Port[[:space:]].*$/Port 9922/' /etc/ssh/sshd_config
-/bin/sed -i 's/port=\"22\"/port=\"9922\"/' /usr/lib/firewalld/services/ssh.xml 
-firewall-cmd --reload
-
-
-###
-# Command History
-###
-function SetHistory()
-{
-	found=`grep -c HISTTIMEFORMAT /etc/profile`
-	if ! [ $found -gt "0" ]
-	then
-	echo "export HISTSIZE=2000" >> /etc/profile
-	echo "export HISTTIMEFORMAT='%F %T:'" >> /etc/profile
-	fi
-}
-
-
 
 function InstallTools()
 {
@@ -193,6 +75,40 @@ function InstallBaseTools()
 	sudo yum install -y  gdisk
 	sudo yum install -y	 net-tools
 	echo 'InstallBaseTools end'
+	echo '----------------------------------'
+}
+
+function InstallVimTools()
+{
+	echo '----------------------------------'
+	echo 'InstallVimTools begin'
+	# install for building Vim
+	sudo yum groupinstall -y "Development Tools"  
+	sudo yum install -y  cmake
+	sudo yum install -y  ctags
+
+	# install lua
+	sudo yum install -y  lua
+	sudo yum install -y  lua-devel
+
+	# install python 
+	sudo yum install -y  python
+	sudo yum install -y  python-devel
+
+	# install perl support
+	sudo yum install -y  perl-ExtUtils-ParseXS
+	sudo yum install -y  perl-ExtUtils-CBuilder
+	sudo yum install -y  perl-ExtUtils-Embed
+
+	# install ncurses
+	sudo yum install -y	 ncurses
+	sudo yum install -y  ncurses-libs
+	sudo yum install -y  ncurses-devel
+
+	# install for build YCM
+	sudo yum install -y  clang
+
+	echo 'InstallVimTools end'
 	echo '----------------------------------'
 }
 
@@ -265,6 +181,68 @@ function PythonEnvs()
 	echo "source /usr/local/bin/virtualenvwrapper.sh" >> $userFolder/.bashrc
 }
 
+function BuildVim()
+{
+	InstallVimTools
+
+	echo '----------------------------------'
+	echo 'BuildVim begin'
+
+	# get latest vim src code 
+	cd "$operatorFolder"
+	git clone https://github.com/vim/vim.git
+
+	cd vim
+	git pull
+	# clean 
+	make distclean  # if you build Vim before
+	
+	# get python path
+	# python_lib_path=$(python -c "from distutils.sysconfig import get_python_lib;import sys; sys.exit(get_python_lib())") 
+	python_lib_path="/usr/lib64/python2.7/config/"	
+
+	# install
+	./configure --with-features=huge \
+	--enable-multibyte \
+	--enable-rubyinterp=yes \
+	--enable-perlinterp=yes \
+	--enable-luainterp=yes \
+	--enable-gui=gtk2 \
+	--enable-cscope \
+	--enable-pythoninterp=yes \
+	--with-python-config-dir=$python_lib_path \
+	--prefix=/usr/local
+	
+	make
+	sudo make install
+
+	# config
+	git clone https://github.com/VundleVim/Vundle.vim.git $userFolder/.vim/bundle/Vundle.vim
+
+	# get vimrc
+	# cd "${operatorFolder}wwsbbase_settings"
+	# cp vimrc $HOME/.vimrc
+	sudo cp "${operatorFolder}wwsbbase_settings/vimrc" $userFolder/.vimrc
+
+	sudo ln -s /usr/local/bin/vim /usr/bin/vim
+	echo 'BuildVim end'
+	echo '----------------------------------'
+}
+
+function BuildYcm()
+{
+	echo '----------------------------------'
+	echo 'BuildYcm begin'
+
+	git clone https://github.com/Valloric/YouCompleteMe.git $userFolder/.vim/bundle/YouCompleteMe
+	cd $userFolder/.vim/bundle/YouCompleteMe
+	git submodule update --init --recursive
+	./install.py --clang-completer
+
+	echo 'BuildYcm end'
+	echo '----------------------------------'
+}
+
 function InstallZlua()
 {
 	echo '----------------------------------'
@@ -277,6 +255,7 @@ function InstallZlua()
 	echo 'InstallZlua end'
 	echo '----------------------------------'
 }
+
 
 function SambaService()
 {
